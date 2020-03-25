25a352d9cd2c941674a60616748e50b8cc62526c
Suhrob Muborakshoev <suhrob.m89@gmail.com>
2020-03-25 15:16:46 +0500

    added crud functionality

--- app.js
+++ app.js
@@ -16,82 +16,91 @@ function loadEventListeners() {
 }
 
 function getTasks() {
-    let tasks;
-    if (localStorage.getItem("tasks") === null) {
-        tasks = [];
-    } else {
-        tasks = JSON.parse(localStorage.getItem("tasks"));
-    }
-
-    tasks.forEach(function(task) {
-        const li = document.createElement("li");
-        li.className = "collection-item";
-        li.appendChild(document.createTextNode(task));
-        const link = document.createElement("a");
-        link.className = "delete-item secondary-content";
-        link.innerHTML = '<i class="fa fa-remove"></li>';
-        li.appendChild(link);
-        taskList.appendChild(li);
-    });
+    fetch('https://vjscript-tasklist.firebaseio.com/tasklist.json')
+        .then(res => {
+            return res.json();
+            /*
+                console.log(res);
+                console.log(res.json());
+                YOU CAN CONSUME RES.JSON() ONLY ONCE. IF YOU CONSUME IT MORE THAN ONCE THE ERROR HAPPENS (IE. COMMENTING OUT THE CONSOLE.LOG(RES.JSON())). THE MSG BODY GETS LOCKED
+            */
+        })
+        .then(data => {
+            addTaskToUI(data);
+        })
+        .catch(err => console.log(err));
 }
 
 function addTask(e) {
     if (taskInput.value === "") {
         alert("Add a task");
+        e.preventDefault();
+        return;
     }
-    const li = document.createElement("li");
-    li.className = "collection-item";
-    li.appendChild(document.createTextNode(taskInput.value));
-    const link = document.createElement("a");
-    link.className = "delete-item secondary-content";
-    link.innerHTML = '<i class="fa fa-remove"></li>';
-    li.appendChild(link);
-    taskList.appendChild(li);
 
-    storeTaskInLocalStorage(taskInput.value);
+    storeTaskInDatabase(taskInput.value);
 
     taskInput.value = "";
-
     e.preventDefault();
 }
 
-function storeTaskInLocalStorage(task) {
-    let tasks;
-    if (localStorage.getItem("tasks") === null) {
-        tasks = [];
-    } else {
-        tasks = JSON.parse(localStorage.getItem("tasks"));
-    }
+function storeTaskInDatabase(task) {
+    fetch('https://vjscript-tasklist.firebaseio.com/tasklist.json', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(task)
+    }).then(res => {
+        return res.json();
+    }).then(data => {
+        let nt = {};//
+        nt[data.name] = task;
+        addTaskToUI(nt);
+    })
+        .catch(err => console.log(err));
+}
 
-    tasks.push(task);
-    localStorage.setItem("tasks", JSON.stringify(tasks));
+function addTaskToUI(data) {
+    let loaderElement = document.getElementById('tasklist-loader');
+    loaderElement.innerHTML = "";
+    loaderElement.classList.remove("lds-dual-ring");
+    if (data) {
+        for (let k in data) {
+            const li = document.createElement("li");
+            li.className = "collection-item";
+            li.appendChild(document.createTextNode(data[k]));
+            const link = document.createElement("a");
+            link.className = "delete-item secondary-content";
+            link.innerHTML = `<i class="fa fa-remove" data-id=${k}></li>`;
+            li.appendChild(link);
+            taskList.appendChild(li);
+        }
+    } else {
+        loaderElement.innerHTML = "The list is empty";
+    }
 }
 
 function removeTask(e) {
     if (e.target.parentElement.classList.contains("delete-item")) {
         if (confirm("Are you sure?")) {
-            e.target.parentElement.parentElement.remove();
-
-            removeTaskFromLocalStorage(e.target.parentElement.parentElement);
+            let itemID = e.target.dataset.id;
+            // console.log(itemID);
+            fetch(`https://vjscript-tasklist.firebaseio.com/tasklist/${itemID}.json`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-type': 'application/json'
+                }
+                //body: JSON.stringify({ takslist: itemID })
+            })
+                .then(res => {
+                    e.target.parentElement.parentElement.remove();
+                    return res.json();
+                })
+                .then(data => resolve(data))
+                .catch(err => console.log(err));
         }
     }
-}
-
-function removeTaskFromLocalStorage(taskItem) {
-    let tasks;
-    if (localStorage.getItem("tasks") === null) {
-        tasks = [];
-    } else {
-        tasks = JSON.parse(localStorage.getItem("tasks"));
-    }
-
-    tasks.forEach(function(task, index) {
-        if (taskItem.textContent === task) {
-            tasks.splice(index, 1);
-        }
-    });
-
-    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 function clearTasks(e) {
@@ -99,17 +108,29 @@ function clearTasks(e) {
         taskList.removeChild(taskList.firstChild);
     }
 
-    clearTasksFromLocalStorage();
+    clearTasksFromServer();
     e.preventDefault();
 }
 
-function clearTasksFromLocalStorage() {
-    localStorage.clear();
+function clearTasksFromServer() {
+    fetch(`https://vjscript-tasklist.firebaseio.com/tasklist.json`, {
+        method: 'DELETE',
+        headers: {
+            'Content-type': 'application/json'
+        }
+        //body: JSON.stringify({ takslist: itemID })
+    })
+        .then(res => {
+            return res.json();
+        })
+        .then(data => resolve(data))
+        .catch(err => console.log(err));
 }
+
 
 function filterTasks(e) {
     const text = e.target.value.toLowerCase();
-    document.querySelectorAll(".collection-item").forEach(function(task) {
+    document.querySelectorAll(".collection-item").forEach(function (task) {
         const item = task.firstChild.textContent;
         if (item.toLowerCase().indexOf(text) != -1) {
             task.style.display = "block";
